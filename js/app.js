eda335a1177e966a538bb75480583eb05cce9e38
Mr-pa-ing <paingphyozaw54@gmail.com>
2021-07-25 10:52:54 +0630

    javascript project 17

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels);
+
+panels.forEach(panel => {
+    // console.log(panel);
+
+
+    panel.addEventListener('mouseenter',()=>{
+        // console.log('hay');
+
+
+        removeactiveprevclass();
+        panel.classList.add('active');
+
+    })
+
+
+    panel.addEventListener('mouseleave',()=>{
+        // console.log('hay');
+
+
+        removeactiveprevclass();
+        
+
+    })
+});
+
+
+function removeactiveprevclass(){
+    panels.forEach((panel)=>{
+        panel.classList.remove('active');
+    })
+}
